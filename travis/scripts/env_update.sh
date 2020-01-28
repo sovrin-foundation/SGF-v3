c097e5f8626614c5282b7b6b9443b0bfcd95bdd1
Vinomaster <dan.gisolfi@gmail.com>
2020-01-28 17:04:53 -0500

    More K8 testing

--- travis/scripts/env_update.sh
+++ travis/scripts/env_update.sh
@@ -10,10 +10,10 @@ echo "Calling helm to upgrade Kubernetes Cluster with latest DockerHub Image..."
 #echo "Successfully upgraded environment STM
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=spu
 #echo "Successfully upgraded environment SPU
-helm upgrade sgfv3-latest helm --namespace default \
+helm upgrade sgfv3-latest helm --namespace default --install \
   --set gfdocprefix=sbu \
   --set image_name=sovrinci/sgf-v3-sbu-docs:latest
-echo "Successfully upgraded environment SBU
+echo "Successfully upgraded environment SBU"
 
 
 exit 0
