7d6c71ae2acb0a8642887f5c0cf76cab240b1350
Vinomaster <dan.gisolfi@gmail.com>
2020-01-30 10:54:54 -0500

    Helm error testing

--- travis/scripts/env_update.sh
+++ travis/scripts/env_update.sh
@@ -10,6 +10,7 @@ echo "Calling helm to upgrade Kubernetes Cluster with latest DockerHub Image..."
 #echo "Successfully upgraded environment STM
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=spu
 #echo "Successfully upgraded environment SPU
+git rev-parse --verify refs/stash
 helm upgrade sgfv3-latest helm --install --namespace default --kubeconfig ./_ci-pipeline/config/kubernetes/boeee1qd0uhgsckare0g.yml -f ./helm/sbu_values.yaml
 echo "Successfully upgraded environment SBU"
 
