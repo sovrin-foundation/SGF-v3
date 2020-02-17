e3531974e5c920bc66fad73e2ebfceeac5d979c8
Vinomaster <dan.gisolfi@gmail.com>
2020-02-17 13:08:28 -0500

    Remove CR K8 Creds

--- travis/scripts/env_update.sh
+++ travis/scripts/env_update.sh
@@ -13,7 +13,7 @@ echo "Calling helm to upgrade Kubernetes Cluster with latest DockerHub Image..."
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=spu
 #echo "Successfully upgraded environment SPU
 #helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig ./_ci-pipeline/config/kubernetes/boeee1qd0uhgsckare0g.yml -f ./helm/sbu_values.yaml
-helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig KUBECONFIG -f ./helm/sbu_values.yaml
+helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig $KUBECONFIG -f ./helm/sbu_values.yaml
 echo "Successfully upgraded environment SBU"
 
 exit 0
