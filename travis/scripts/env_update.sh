5a00a9c948f7d754c47f481263e1d872c8c27b6c
Vinomaster <dan.gisolfi@gmail.com>
2020-02-17 11:49:20 -0500

    Test dynamic auth for K8

--- travis/scripts/env_update.sh
+++ travis/scripts/env_update.sh
@@ -5,12 +5,15 @@
 
 set -e
 
+export KUBECONFIG=$HOME/.bluemix/plugins/container-service/clusters/boeee1qd0uhgsckare0g/kube-config-dal10-sovrin-metasystem-gf.yml
+
 echo "Calling helm to upgrade Kubernetes Cluster with latest DockerHub Image..."
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=stm
 #echo "Successfully upgraded environment STM
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=spu
 #echo "Successfully upgraded environment SPU
-helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig ./_ci-pipeline/config/kubernetes/boeee1qd0uhgsckare0g.yml -f ./helm/sbu_values.yaml
+#helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig ./_ci-pipeline/config/kubernetes/boeee1qd0uhgsckare0g.yml -f ./helm/sbu_values.yaml
+helm upgrade sgfv3-latest helm --install --force --namespace default --kubeconfig KUBECONFIG -f ./helm/sbu_values.yaml
 echo "Successfully upgraded environment SBU"
 
 exit 0
