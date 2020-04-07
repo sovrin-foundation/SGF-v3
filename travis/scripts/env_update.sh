3d4479e6150a124a506d9df740def76535164c1f
Vinomaster <dan.gisolfi@gmail.com>
2020-04-07 17:33:24 -0400

    helm $USER test

--- travis/scripts/env_update.sh
+++ travis/scripts/env_update.sh
@@ -5,7 +5,8 @@
 
 set -e
 
-export KUBECONFIG=$HOME/.bluemix/plugins/container-service/clusters/boeee1qd0uhgsckare0g/kube-config-dal10-sovrin-metasystem-gf.yml
+#export KUBECONFIG=$HOME/.bluemix/plugins/container-service/clusters/boeee1qd0uhgsckare0g/kube-config-dal10-sovrin-metasystem-gf.yml
+export KUBECONFIG=$USER/.bluemix/plugins/container-service/clusters/boeee1qd0uhgsckare0g/kube-config-dal10-sovrin-metasystem-gf.yml
 
 echo "Calling helm to upgrade Kubernetes Cluster with latest DockerHub Image..."
 #helm upgrade sgfv3-latest ../../helm --namespace default --set gfdocprefix=stm
